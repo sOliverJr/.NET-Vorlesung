f336f32e41fb8c541d31b7b2b182e65356121471
Oliver Seider <64847610+sOliverJr@users.noreply.github.com>
2023-06-01 12:37:51 +0200

    Erste Vorlesung

---
+++ Erste Vorlesung/Aufgabe_3/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hallo, gib einen Satz ein!");
+
+string input = Console.ReadLine();
+
+char[] charArray = input.ToCharArray();
+
+for (int i = charArray.Length; i > 0; i--)
+{
+    Console.Write(charArray[i-1]);
+}
+Console.WriteLine("");
+Console.WriteLine("Done!");
+Console.ReadLine();
---
+++ Erste Vorlesung/Aufgabe_4/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.Write("Zahl 1:");
+string string1 = Console.ReadLine();
+Console.Write("Zahl 2:");
+string string2 = Console.ReadLine();
+Console.Write("Operator:");
+string op = Console.ReadLine();
+
+int zahl1 = Int16.Parse(string1);
+int zahl2 = Int16.Parse(string2);
+
+Console.Write("Ergebnis: ");
+if (op == "*")
+{
+    Console.WriteLine(zahl1 * zahl2);
+}
+else if (op == "+")
+{
+    Console.WriteLine(zahl1 + zahl2);
+}
+else if (op == "-")
+{
+    Console.WriteLine(zahl1 - zahl2);
+}
+Console.ReadLine();
